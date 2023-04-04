044422becccc4662f6699e09f56bf15e9524653a
Kartikey Srivastava <Kartikey.Srivastava@hanu.com>
2023-04-04 11:46:22 +0530

    cache

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions</h1>
+        <h1>Learn & Master GitHub Action</h1>
       </header>
       <MainContent />
     </>
